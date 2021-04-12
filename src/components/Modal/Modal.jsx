06db4147a9acf0edaa6484d25e747b7fd0e89d75
Matthias <matthias.gander@braineet.com>
2021-04-12 16:05:31 +0200

    add trends csv and display it

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,67 @@
-import React from "react";
-// import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import CanvasJSReact from "../../lib/canvasjs.react";
+import * as d3 from "d3";
+
+import csvBitcoin from "../../data/bitcoin.csv";
+import csvLitecoin from "../../data/litecoin.csv";
+import csvRipple from "../../data/ripple.csv";
+import csvDogecoin from "../../data/dogecoin.csv";
+import csvEthereum from "../../data/ethereum.csv";
+
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const Modal = ({ isOpen, onClose, crypto }) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getCsv = {
+      Bitcoin: csvBitcoin,
+      Ripple: csvRipple,
+      Litecoin: csvLitecoin,
+      Ethereum: csvEthereum,
+      DogeCoin: csvDogecoin,
+    };
+    if (crypto) {
+      d3.csv(getCsv[crypto.title]).then((d) => {
+        const dataPoints = [];
+        d.forEach((point) => {
+          dataPoints.push({
+            x: new Date(point[d.columns[0]]),
+            y: parseInt(point[d.columns[1]]),
+          });
+        });
+        setData(dataPoints);
+      });
+    }
+  }, [crypto]);
+
   if (!isOpen) {
     return null;
   }
 
+  const options = {
+    title: {
+      text: `${crypto.title}, his popularity against his price`,
+    },
+    axisY: {
+      title: "% of price and % of popularity",
+    },
+    data: [
+      {
+        type: "spline",
+        dataPoints: data,
+        showInLegend: true,
+      },
+    ],
+  };
+
   return (
     <div className="modal-container">
       <div className="back" onClick={onClose}></div>
-      <div className="modal"></div>
+      <div className="modal">
+        <CanvasJSChart options={options} />
+      </div>
     </div>
   );
 };
